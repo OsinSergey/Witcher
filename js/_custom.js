5c0f70a6f387508eac2ca4afbf74f4d606f71fe3
Osin Sergey <osin_osin@mail.ru>
2020-02-07 14:32:04 +0600

    Add files via upload

---
+++ js/_custom.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let mySwiper = new Swiper('.swiper-container', {
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+			nextEl: '.arrow',
+		},
+		breakpoints: {
+			// when window width is >= 320px
+			580: {
+				slidesPerView: 2,
+
+			},
+		},
+	});
+
+	let menuBtn = document.querySelector('.menu-btn');
+	let menu = document.querySelector('.header');
+	menuBtn.addEventListener('click', () => {
+		menu.classList.toggle('header-active');
+		menuBtn.classList.toggle('menu_active');
+	});
+
+});
